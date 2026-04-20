0ba0db3054165c9ff79eeae1b0b45ab6ddcf31db
Sagar Verma <sagarvermasagarverma07@gmail.com>
2026-04-20 11:31:21 +0530

    prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,11 @@ app.use(express.json());
 
 // PostgreSQL connection
 const pool = new Pool({
-  user: 'postgres',
-  host: 'db',
-  database: 'event_management',
-  password: '1234',  // put your password
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // Test route
